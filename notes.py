2985d7b4d07d4c8dc939e2bf73468fbba2ee8edb
ghoshrohit72 <ghoshrohit72@gmail.com>
2020-05-31 22:22:35 +0530

    Initial Commit (uploading all django files)

---
+++ notes.py
@@ -0,0 +1,10 @@
+'''
+Create a working basic twopage web app ======DONE========
+Add a navbar to include home, mainframe,register, login and logout ======DONE========
+Use base.html to extend and declutter the main html files ======DONE========
+Create a working registration form ======DONE========
+Create a working login page and logout button  ======DONE========
+Making the mainframe page login required ======DONE========
+Improve the navbar with formatting and logout appear only when log in ======DONE========
+
+'''
